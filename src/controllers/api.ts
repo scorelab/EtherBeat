a00a8e808b46ceedfd060de33eb134e839c27a11
prabushitha <prabushitha@gmail.com>
2018-04-26 14:47:15 +0530

    fixed compilation failures due to tslint errors

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,7 +2,7 @@
 
 import * as async from "async";
 import * as request from "request";
-import {Response, Request, NextFunction} from "express";
+import { Response, Request, NextFunction } from "express";
 
 
 /**
