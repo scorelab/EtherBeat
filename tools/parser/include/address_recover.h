2c7f8596673374d860875aaf7e4fbd61e77fee88
prabushitha <prabushitha@gmail.com>
2018-05-30 00:55:54 +0530

    remove unwanted comments

--- tools/parser/include/address_recover.h
+++ tools/parser/include/address_recover.h
@@ -4,6 +4,5 @@
 #include <vector>
 
 secp256k1_context const* getCtx();
-// std::vector<uint8_t> recover(std::vector<uint8_t> _sig, std::vector<uint8_t> _message);
 std::vector<uint8_t> recover(uint8_t v_new, std::vector<uint8_t> r, std::vector<uint8_t> s, std::vector<uint8_t> _message);
 std::vector<uint8_t> publicKeyToAddress(std::vector<uint8_t> public_key);
--- tools/parser/src/address_recover.cpp
+++ tools/parser/src/address_recover.cpp
@@ -10,7 +10,7 @@ secp256k1_context const* getCtx()
 	return s_ctx.get();
 }
 
-// A signature: 65 bytes: r: [0, 32), s: [32, 64), v: 64.
+
 // Message is 32 bit hash of the tx/message
 std::vector<uint8_t> recover(uint8_t v_new, std::vector<uint8_t> r, std::vector<uint8_t> s, std::vector<uint8_t> _message)
 {
@@ -22,7 +22,8 @@ std::vector<uint8_t> recover(uint8_t v_new, std::vector<uint8_t> r, std::vector<
 		return {};
 	}
 
-	//concat r and s with 32 in size (leading 0s)
+	// A signature: 65 bytes: r: [0, 32), s: [32, 64), v: 64.
+	//concat r and s with 32 in size (leading 0s if size mismatch)
 	std::vector<uint8_t> signature;
 	signature.reserve( 65 );                // preallocate memory
 	// left pad 0 for r
@@ -65,43 +66,7 @@ std::vector<uint8_t> recover(uint8_t v_new, std::vector<uint8_t> r, std::vector<
 	// Create the Public key skipping the header.
 	return std::vector<uint8_t> (serializedPubkey.begin()+1, serializedPubkey.end());
 }
-/*
-std::vector<uint8_t> recover(std::vector<uint8_t> _message, std::vector<uint8_t>  _sig)
-{
-    std::vector<uint8_t> public_key;
-	int v = _sig[64]-27;
 
-	if (v > 3 || v < 0){
-	    printf("Invalid Recid: %d\n", v);
-		return {};
-	}
-
-	auto* ctx = getCtx();
-	secp256k1_ecdsa_recoverable_signature rawSig;
-	if (!secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rawSig, (uint8_t *)&_sig[0], v)){
-	    printf("No secp256k1_ecdsa_recoverable_signature\n");
-	    return {};
-	}
-
-	secp256k1_pubkey rawPubkey;
-	if (!secp256k1_ecdsa_recover(ctx, &rawPubkey, &rawSig, (uint8_t *)&_message[0])){
-	     printf("Couldn't recover public key\n");
-	     return {};
-	}
-
-
-	std::array<uint8_t, 65> serializedPubkey;
-	size_t serializedPubkeySize = serializedPubkey.size();
-	secp256k1_ec_pubkey_serialize(
-			ctx, serializedPubkey.data(), &serializedPubkeySize,
-			&rawPubkey, SECP256K1_EC_UNCOMPRESSED
-	);
-	// // assert(serializedPubkeySize == serializedPubkey.size());
-	// Expect single byte header of value 0x04 -- uncompressed public key.
-	// // assert(serializedPubkey[0] == 0x04);
-	// Create the Public key skipping the header.
-	return std::vector<uint8_t> (serializedPubkey.begin()+1, serializedPubkey.end());
-}*/
 
 std::vector<uint8_t> publicKeyToAddress(std::vector<uint8_t> public_key) {
     std::vector<uint8_t> pubkey_keccak = keccak_256(public_key);
