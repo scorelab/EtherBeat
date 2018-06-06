57c5a5b382e27c8ab4e95c2e85af0ce41a4fd19d
prabushitha <prabushitha@gmail.com>
2018-06-06 19:26:40 +0530

    utils unit tests done

--- tools/parser/include/utils.h
+++ tools/parser/include/utils.h
@@ -4,21 +4,36 @@
 #include <vector>
 #include <string>
 
-std::vector<uint8_t> toBigEndianEightBytes(uint64_t number);
+
+// returns a byte string (used to create the leveldb key)
 std::string getKeyString(uint8_t prefix[], uint8_t middle[], uint8_t suffix[], int prefixSize=0, int middleSize=0, int suffixSize=0);
-void print_bytes (std::string, std::string heading="Bytes");
-void print_bytes (std::vector<uint8_t> val, std::string heading="Bytes");
-std::string hexStr(unsigned char *, int);
 
-int hexStringToInt(std::string);
-size_t bytesVectorToInt(std::vector<uint8_t> bytes_arr);
-
+// returns a byte vector from a string (to convert the response from leveldb to bytes)
 std::vector<uint8_t> getByteVector(std::string byteString);
 
+
+// converts a number to big endian notation with 8 bytes
+std::vector<uint8_t> toBigEndianEightBytes(uint64_t number);
+
+// converts an byte array (pointed to the 1st byte address) to a hexadecimal notation string
+std::string hexStr(unsigned char *, int);
+
+//
+int hexStringToInt(std::string);
+
+// assembles a byte vector to its integer notation
+size_t bytesVectorToInt(std::vector<uint8_t> bytes_arr);
+
+// returns keccak_256 hash (in byte form) to a given bytes vector
 std::vector<uint8_t> keccak_256(std::vector<uint8_t > &input);
 
+// returns a byte vector from hexadecimal notation
 std::vector<uint8_t> hex_to_bytes(std::string const& hex);
 
+// returns a string without leading 0x (basically to remove 0x from user given hexadecimal string)
 std::string remove0xFromString(std::string hex_str);
 
+// printing
+void print_bytes (std::string, std::string heading="Bytes");
+void print_bytes (std::vector<uint8_t> val, std::string heading="Bytes");
 
--- tools/parser/test/utils.cpp
+++ tools/parser/test/utils.cpp
@@ -66,7 +66,7 @@ TEST(Keystring, PrefixMiddleSuffix) {
     EXPECT_EQ(key, "\001\002\003\001\003\004\005");
 }
 
-TEST(HexString, PrefixMiddleSuffix) {
+TEST(HexString, ByteVectorToHex) {
     // bytes array to hex string
     std::vector<uint8_t> bytes_arr;
     bytes_arr.insert(bytes_arr.end(), 0);
@@ -76,4 +76,109 @@ TEST(HexString, PrefixMiddleSuffix) {
 
     std::string str = hexStr(&bytes_arr[0], bytes_arr.size());
     EXPECT_EQ(str, "0000031c");
+}
+
+TEST(HexString, ByteVectorToHexEmpty) {
+    // bytes array to hex string
+    std::vector<uint8_t> bytes_arr;
+
+    std::string str = hexStr(&bytes_arr[0], bytes_arr.size());
+    EXPECT_EQ(str, "");
+}
+TEST(HexString, ByteVectorToHexZero) {
+    // bytes array to hex string
+    std::vector<uint8_t> bytes_arr;
+    bytes_arr.insert(bytes_arr.end(), 0);
+
+    std::string str = hexStr(&bytes_arr[0], bytes_arr.size());
+    EXPECT_EQ(str, "00");
+}
+
+TEST(bytesVectorToInt, EmptyByteVector) {
+    // bytes array to hex string
+    std::vector<uint8_t> bytes_arr;
+
+    size_t val = bytesVectorToInt(bytes_arr);
+
+    EXPECT_EQ(val, 0);
+}
+
+TEST(bytesVectorToInt, ByteVector) {
+    // bytes array to hex string
+    std::vector<uint8_t> bytes_arr;
+    bytes_arr.insert(bytes_arr.end(), 0);
+    bytes_arr.insert(bytes_arr.end(), 0);
+    bytes_arr.insert(bytes_arr.end(), 0);
+    bytes_arr.insert(bytes_arr.end(), 0);
+    bytes_arr.insert(bytes_arr.end(), 0);
+    bytes_arr.insert(bytes_arr.end(), 0);
+    bytes_arr.insert(bytes_arr.end(), 1);
+    bytes_arr.insert(bytes_arr.end(), 3);
+
+    size_t val = bytesVectorToInt(bytes_arr);
+
+    EXPECT_EQ(val, 259);
+}
+
+TEST(BytesVector, EmptyByteString) {
+    std::string str = "";
+    std::vector<uint8_t> bytes_arr = getByteVector(str);
+    EXPECT_EQ(bytes_arr.size(), 0);
+}
+
+TEST(BytesVector, ValidByteString) {
+    std::string str = "a23";
+    std::vector<uint8_t> bytes_arr = getByteVector(str);
+    EXPECT_EQ(bytes_arr.size(), 3);
+
+    EXPECT_EQ(bytes_arr[0], 97);
+    EXPECT_EQ(bytes_arr[1], 50);
+    EXPECT_EQ(bytes_arr[2], 51);
+}
+
+TEST(Keccak256, OneElement) {
+    std::vector<uint8_t> inp;
+    inp.insert(inp.end(), 1);
+    std::vector<uint8_t> bytes_arr = keccak_256(inp);
+    std::string hashHex = hexStr(&bytes_arr[0], bytes_arr.size());
+
+    EXPECT_EQ(hashHex, "5fe7f977e71dba2ea1a68e21057beebb9be2ac30c6410aa38d4f3fbe41dcffd2");
+}
+
+TEST(Hex2Bytes, emptyHex) {
+    std::string hex_str = "";
+    std::vector<uint8_t> bytes_arr = hex_to_bytes(hex_str);
+
+    EXPECT_EQ(bytes_arr.size(), 0);
+}
+
+TEST(Hex2Bytes, validHex) {
+    std::string hex_str = "00000012";
+    std::vector<uint8_t> bytes_arr = hex_to_bytes(hex_str);
+
+    EXPECT_EQ(bytes_arr.size(), 4);
+    EXPECT_EQ(bytes_arr[0], 0);
+    EXPECT_EQ(bytes_arr[1], 0);
+    EXPECT_EQ(bytes_arr[2], 0);
+    EXPECT_EQ(bytes_arr[3], 18);
+}
+
+TEST(Remove0xFromString, HexWithLeading0x) {
+    std::string hex_str = "0x00000012";
+    std::string res = remove0xFromString(hex_str);
+
+    EXPECT_EQ(res, "00000012");
+}
+TEST(Remove0xFromString, InvalidHexWithMid0x) {
+    std::string hex_str = "000x000012";
+    std::string res = remove0xFromString(hex_str);
+
+    EXPECT_EQ(res, "000x000012");
+}
+
+TEST(Remove0xFromString, HexWithNo0x) {
+    std::string hex_str = "00000012";
+    std::string res = remove0xFromString(hex_str);
+
+    EXPECT_EQ(res, "00000012");
 }
