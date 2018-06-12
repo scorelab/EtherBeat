88b74932f393fed799b8eb733675768ead2e1215
prabushitha <prabushitha@gmail.com>
2018-06-13 01:38:15 +0530

    integrated sqlite

--- tools/EtherBuilder/main.cpp
+++ tools/EtherBuilder/main.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
-#include "leveldb/db.h"
 #include "parser.h"
+#include <sqlite3.h>
+#include <iostream>
+#include <stdio.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    leveldb::DB* db;
-    leveldb::Options options;
-    options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "/home/prabushitha/.ethereum/rinkeby/geth/chaindata", &db);
 
-    Parser p(db);
-    Block b = p.getBlock(100);
-    b.print();
+    sqlite3 *db;
+    int rc = sqlite3_open("ethereum_blockchain.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+
+    Parser p("/home/prabushitha/.ethereum/rinkeby/geth/chaindata");
+
+    Block b = p.getBlock(55);
+    // b.print();
+
+
+    sqlite3_close(db);
     return 0;
 }
