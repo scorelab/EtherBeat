453213f90739930c7c88b8fdd3ead77d95fa8bce
prabushitha <prabushitha@gmail.com>
2018-08-07 22:53:02 +0530

    docker file changes

---
+++ tools/EtherBuilder/docker_setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# dependencies
+# leveldb
+sudo apt-get install libsnappy-dev
+
+wget https://leveldb.googlecode.com/files/leveldb-1.9.0.tar.gz
+tar -xzf leveldb-1.9.0.tar.gz
+cd leveldb-1.9.0
+make
+sudo mv libleveldb.* /usr/local/lib
+cd include
+sudo cp -R leveldb /usr/local/include
+sudo ldconfig
+
+#cryptopp
+apt-get install libcrypto++ libcrypto++-dev libcrypto++9v5-dbg libcrypto++-utils
+
+#secp256k1
+sudo apt-get install autoconf autogen libtool
+git clone https://github.com/bitcoin-core/secp256k1.git
+cd secp256k1
+./autogen.sh
+./configure
+make
+./tests
+sudo make install  # optional
+
+# gtest
+sudo apt-get install libgtest-dev
+
+# project
+git clone https://github.com/prabushitha/EtherBeat.git
+cd EtherBeat
+git checkout parser
+cd tools/EtherExtractor
+chmod +x run.sh
+./run
