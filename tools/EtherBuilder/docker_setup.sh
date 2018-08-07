fbf5e85b7124b87872eeef50077263118bf3006b
prabushitha <prabushitha@gmail.com>
2018-08-07 23:32:24 +0530

    removed unwanted lines

--- tools/EtherBuilder/docker_setup.sh
+++ tools/EtherBuilder/docker_setup.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
 # dependencies
-# leveldb
-# sudo apt-get install libsnappy-dev
-
-wget https://leveldb.googlecode.com/files/leveldb-1.9.0.tar.gz
-tar -xzf leveldb-1.9.0.tar.gz
-cd leveldb-1.9.0
-make
-sudo mv libleveldb.* /usr/local/lib
-cd include
-sudo cp -R leveldb /usr/local/include
-sudo ldconfig
 
 #secp256k1
 # sudo apt-get install autoconf autogen libtool
